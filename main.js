f02ee39e7e358c0a3d35cdc031d33c3053afa17a
RRAJ644 <riturajsingh.17248@gmail.com>
2024-11-19 13:22:50 +0530

    getRequest

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-const https = require('https');
+const https = require('https')
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
 // TODO: Implement this function
+const server = https
+  .createServer((req, res) => {
+    https.get(url, (res) => {
+      let data = ''
+
+      res.on('end', () => {
+        try {
+          const jsonResponse = JSON.parse(data)
+          console.log(`Title: ${jsonResponse.title}`)
+        } catch (error) {
+          console.log('Error parsing JSON:', error.message)
+        }
+      })
+    })
+  })
+  .on('error', (err) => {
+    console.log('Request failed:', err.message)
+  })
+
+server.listen(300, () => {
+  console.log('server is listening at port 3000')
+})
