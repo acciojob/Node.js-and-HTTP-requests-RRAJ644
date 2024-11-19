b8345b8a72525e6c1d51458497887142f88c22a5
RRAJ644 <riturajsingh.17248@gmail.com>
2024-11-19 13:35:19 +0530

    main js

--- main.js
+++ main.js
@@ -1,39 +1,24 @@
 const https = require('https')
 
-// URL to fetch data from
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
-// Create an HTTP server
-const server = https.createServer((req, res) => {
-  // Perform the GET request
-  https
-    .get(url, (response) => {
-      let data = ''
+https
+  .get(url, (res) => {
+    let data = ''
 
-      // Collect data chunks
-      response.on('data', (chunk) => {
-        data += chunk
-      })
-
-      // Process and send the data once fully received
-      response.on('end', () => {
-        try {
-          const jsonResponse = JSON.parse(data)
-          res.writeHead(200, { 'Content-Type': 'application/json' })
-          res.end(JSON.stringify({ title: jsonResponse.title }))
-        } catch (error) {
-          res.writeHead(500, { 'Content-Type': 'text/plain' })
-          res.end('Error parsing JSON')
-        }
-      })
+    res.on('data', (chunk) => {
+      data += chunk
     })
-    .on('error', (err) => {
-      res.writeHead(500, { 'Content-Type': 'text/plain' })
-      res.end('Request failed: ' + err.message)
-    })
-})
 
-// Listen on port 3000
-server.listen(3000, () => {
-  console.log('Server is listening on port 3000')
-})
+    res.on('end', () => {
+      try {
+        const todo = JSON.parse(data)
+        console.log(todo.title)
+      } catch (error) {
+        console.error('Error parsing JSON:', error.message)
+      }
+    })
+  })
+  .on('error', (error) => {
+    console.error('Error fetching todo:', error.message)
+  })
